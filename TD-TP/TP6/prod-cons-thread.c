c42696fe7c9f5ee0b1fda8db0f721fc07780f6d9
Jicquel Vincent <vincent.jicquel@gmail.com>
2016-10-28 20:48:09 +0200

    finition prod-cons-thread

--- TD-TP/TP6/prod-cons-thread.c
+++ TD-TP/TP6/prod-cons-thread.c
@@ -3,14 +3,54 @@
 #include <pthread.h>
 #define L 15
 
+pthread_mutex_t m;
+pthread_cond_t noplaces=PTHREAD_COND_INITIALIZER, nobonbons=PTHREAD_COND_INITIALIZER;
 
-int main(void){
 int places=L;
 int bonbons=0;
 
-pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+void * prod(void* arg){
+  while(1){
+    while(places<1)
+      pthread_cond_wait(&noplaces,&m);
+    places--;
+    bonbons++;
 
-pthread_mutex_t m;
+    printf("producer (%d) -- put a candy -- %d\n",getpid(),bonbons);
+    sleep(rand()%3);
+
+    pthread_cond_signal(&nobonbons);
+  }
+}
 
 
+void * cons(void* arg){
+  while(1){
+    while(bonbons<1)
+      pthread_cond_wait(&nobonbons,&m);
+
+    bonbons--;
+    places++;
+
+    printf("  cons (%d) -- get a candy -- %d\n",getpid(),bonbons);
+    sleep(rand()%3);
+
+    pthread_cond_signal(&noplaces);
+  }
+}
+
+
+int main(void){
+
+  pthread_t conso[2];
+  pthread_t produ[2];
+  int i;
+
+  for(i=0;i<2;i++){
+    pthread_create(&produ[i],NULL,prod,NULL);
+    pthread_create(&conso[i],NULL,cons,NULL);
+  }
+  while(1){
+    sleep(2);
+  }
 }
