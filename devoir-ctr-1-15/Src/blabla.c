61909587e3b45de7dde3b6f0c0d767ec88b4e380
Jicquel Vincent <vincent.jicquel@gmail.com>
2016-11-06 10:15:17 +0100

    controle asr

---
+++ devoir-ctr-1-15/Src/blabla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  while (1) {
+     printf("Bla-bla-bla?\n"); 
+     printf("Bla-bla-bla!\n");
+     printf("Bla-bla-bla...\n");
+     sleep(2);
+  }
+}
+
