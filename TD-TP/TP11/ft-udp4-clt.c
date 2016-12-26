88e09c2c5567e88df15c48bc3d6ab8de29ab6cc3
Jicquel Vincent <vincent.jicquel@gmail.com>
2016-12-26 14:33:29 +0100

    udp fonctionnel

--- TD-TP/TP11/ft-udp4-clt.c
+++ TD-TP/TP11/ft-udp4-clt.c
@@ -6,40 +6,55 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 
+#define MAXSIZE 65536 //2^16
 
 int main(int argc, char** argv){
   int idS=socket(AF_INET,SOCK_DGRAM,0);
-  int yes=1;
+  int yes=1, nbByte;
+  char buff[MAXSIZE];
+  memset(buff,0,MAXSIZE);
 
-  if(argc != 2)
+  if(argc != 4)
   {
-    perror("executable <addr dest>");
+    perror("executable <file name> <taille d'une tranche> <addr dest>");
     exit(1);
   }
 
-  if(setsockopt(idS,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int))==-1){
+  int fd = open(argv[1],O_RDONLY,0644);
+  int tranche = atoi(argv[2]);
+  printf("%d", tranche);
+
+  if(setsockopt(idS,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int))==-1)
+  {
     perror("setsockopt");
     exit(1);
   }
 
+  
   struct sockaddr_in remote;
   memset(&remote,0,sizeof(struct sockaddr_in));
   remote.sin_port = htons((int16_t) 6666);
   remote.sin_family=AF_INET;
-  inet_pton(AF_INET,argv[1],&(remote.sin_addr.s_addr));
+  inet_pton(AF_INET,argv[3],&(remote.sin_addr.s_addr));
 
-  if(connect(idS,(struct sockaddr*)&remote,sizeof(remote))==-1){
+  if(connect(idS,(struct sockaddr*)&remote,sizeof(remote))==-1)
+  {
     perror("connect");
     exit(1);
   }
 
-  while(1)
+  nbByte = read(fd,(void*) buff,(size_t) tranche);
+
+  while(nbByte != 0)
   {
-    sendto(idS,"salut",6,0,(struct sockaddr*) &remote, sizeof(remote));
-    printf("envoi\n");
-    sleep(1);
+    sendto(idS,buff,nbByte,0,(struct sockaddr*) &remote, sizeof(remote));
+    printf("envoi de %d byte\n",nbByte);
+    memset(buff,0,MAXSIZE);
+  nbByte = read(fd,(void*) buff,(size_t) tranche);
   }
+  printf("Fini\n");
   return EXIT_SUCCESS;
 }
 
--- TD-TP/TP11/ft-udp4-srv.c
+++ TD-TP/TP11/ft-udp4-srv.c
@@ -1,29 +1,43 @@
 #include <sys/socket.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/un.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 
 #define MAXSIZE 256
 
 int main(int argc, char** argv)
 {
 
+  if(argc!=3)
+  {
+    perror("./exe <port> <file written>");
+    exit(1);
+  }
+
   int idS=socket(AF_INET,SOCK_DGRAM,0);
   int yes,nbBytes=0;
   char buff[MAXSIZE];
-
+  int16_t port = (int16_t) atoi(argv[1]);
   struct sockaddr_in local;
   socklen_t sizeLocal = sizeof(local);
+
+  int fd = open(argv[2], O_CREAT | O_WRONLY, 0644);
+
+  if(fd == -1)
+  {
+    perror("open");
+    exit(1);
+  }
 
   memset(buff,0,MAXSIZE);
   socklen_t lenghtSockaddr = sizeof(struct sockaddr);;
 
   memset(&local,0,sizeof(struct sockaddr_in));
-  local.sin_port = htons((int16_t) 6666);
+  local.sin_port = htons(port);
   local.sin_family = AF_INET;
   local.sin_addr.s_addr = INADDR_ANY;
 
@@ -46,7 +60,7 @@ int main(int argc, char** argv)
       perror("recvfrom");
       exit(1);
     }
-    printf("ok\n");
-    printf("%s",buff);
+    write(fd,buff,nbBytes);
+    printf("reception %d bytes\n",nbBytes);
   }
 }
