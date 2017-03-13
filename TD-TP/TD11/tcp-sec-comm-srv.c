4612bd23694a20cd1b6fa82792e4538c4d155365
root <root@raspberrypi.(none)>
2017-03-13 22:00:19 +0000

    bug à résoudre

--- TD-TP/TD11/tcp-sec-comm-srv.c
+++ TD-TP/TD11/tcp-sec-comm-srv.c
@@ -8,6 +8,16 @@
 #include <fcntl.h>
 
 #define MAXBUF 500
+void tea_decrypt(unsigned int v[2], unsigned int k[4])
+{
+  unsigned int delta = 0x9e3779b9;
+  int i;
+  for(i=32;i>0;i--)
+  {
+    v[1]-=(k[2]+(v[0]<<4))^(v[0]*(delta*i))^(k[3]+(v[0]>>5));
+    v[0]-=(k[0]+(v[1]<<4))^(v[1]*(delta*i))^(k[1]+(v[1]>>5));
+  }
+}
 
 unsigned int exponen(unsigned int g, unsigned int exposant, unsigned int p)
 {
@@ -32,7 +42,6 @@ int main(int argc, char** argv){
   int nIdS,x,nbBytes;
   socklen_t sizelocal = sizeof(struct sockaddr_in);
   int yes=1;
-  char c[MAXBUF];
   int port;
 
   if(argc != 2)
@@ -79,7 +88,7 @@ int main(int argc, char** argv){
   unsigned int p,g,xb,yb,ya,zb;
   unsigned int k[4];
 
-/***reception de p***/
+  /***reception de p***/
   recv(nIdS,(void*) &p,4,0);
   send(nIdS,(void*) &p,4,0);
 
@@ -87,40 +96,47 @@ int main(int argc, char** argv){
   recv(nIdS,(void*) &g,4,0);
   send(nIdS,(void*) &g,4,0);
 
-/***choix secret xb***/
+  /***choix secret xb***/
   xb = (unsigned int) (rand()%(p-2))+2;
 
   /***calcul yb***/
   yb = exponen(g,xb,p);
-  
+
   /***envoi de yb***/
   send(nIdS,(void*) &yb,4,0);
 
-/***reception de ya***/
+  /***reception de ya***/
   recv(nIdS,(void*) &ya,4,0);
 
   /***calcul zb***/
   zb = exponen(ya,xb,p);
-  
+
   /***construction key k***/
   k[0] = ~zb;
   k[1] = k[0] & 16383;
   k[2] = k[1] | 16383;
   k[3] = zb;
 
+  char buff[8];
+  unsigned int v[2];
+
   while(1)
   {
-    nbBytes=recv(nIdS,c,MAXBUF,0);
+    nbBytes=recv(nIdS,v,8,0);
+    tea_decrypt(v,k);
+    printf("%d",nbBytes);
+
     if(nbBytes<=0)
     {
       perror("connexion terminée");
       exit(1);
     }
-    printf("nbByte : %d\n",nbBytes);
-    memset(c,0,MAXBUF);
+    bcopy((void*)v,(void*)buff,8);
+    printf("%s",buff);
+    printf("%d",strlen(buff));
+    memset((void*)v,0,8);
+    memset(buff,0,8);
   }
 
-
-
-return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
