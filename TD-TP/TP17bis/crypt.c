82c4d31e0f044a0b37a7337a580dce25c561fa48
jicquel <vincent.jicquel@orange.fr>
2017-03-26 17:09:04 +0200

    correction permutations

--- TD-TP/TP17bis/crypt.c
+++ TD-TP/TP17bis/crypt.c
@@ -4,102 +4,109 @@
 
 unsigned char encrypt_block(unsigned char  block, unsigned short key, unsigned char substitutions[RANGE_VALUE_SUBST], unsigned char permutations[BLOCK_SIZE])
 {
-  unsigned char k[2];
+	unsigned char k[2];
 
-  memcpy((void*)k,(void*)&key,2*sizeof(unsigned char));
+	memcpy((void*)k,(void*)&key,2*sizeof(unsigned char));
 
-  unsigned char blockTmp = 0;
-  unsigned char mask =0;
+	unsigned char blockTmp = 0;
+	unsigned char mask =0;
+	unsigned char valeurIndiceJ=0;
+	int j;
 
-  block = block ^ k[0];
+	block = block ^ k[0];
 
-  //SUBSTITUTIONS
-  blockTmp = 0;
-  mask = 0xf;
-  blockTmp |= substitutions[(block & mask)];
-  mask = mask << 4;
-  blockTmp |= (substitutions[(block & mask)>>4] << 4);
-  block = blockTmp;
+	//SUBSTITUTIONS
+	blockTmp = 0;
+	mask = 0xf;
+	blockTmp |= substitutions[(block & mask)];
+	mask = mask << 4;
+	blockTmp |= (substitutions[(block & mask)>>4] << 4);
+	block = blockTmp;
 
-  //PERMUTATIONS
-  int j;
-  blockTmp = 0;
-  mask = 0x1;
-  for(j=0;j<BLOCK_SIZE;j++)
-  {
-    blockTmp |= (block & (mask << permutations[j]));
-  }
-  block = blockTmp;
-  block = block ^ k[1];
+	//PERMUTATIONS
+	blockTmp = 0;
+	mask = 0x1;
 
-  //SUBSTITUTIONS
-  blockTmp = 0;
-  mask = 0xf;
-  blockTmp |= substitutions[(block & mask)];
-  mask = mask << 4;
-  blockTmp |= (substitutions[(block & mask)>>4] << 4);
-  block = blockTmp;
+	for(j=0;j<BLOCK_SIZE;j++)
+	{
+		valeurIndiceJ=(block & (mask << j)) >> j;
+		blockTmp |= valeurIndiceJ << permutations[j];
+	}
+	block = blockTmp;
+	block = block ^ k[1];
 
-  //PERMUTATIONS
-  blockTmp = 0;
-  mask = 0x1;
-  for(j=0;j<8;j++)
-  {
-    blockTmp |= (block & (mask << permutations[j]));
-  }
-  block = blockTmp;
+	//SUBSTITUTIONS
+	blockTmp = 0;
+	mask = 0xf;
+	blockTmp |= substitutions[(block & mask)];
+	mask = mask << 4;
+	blockTmp |= (substitutions[(block & mask)>>4] << 4);
+	block = blockTmp;
 
-  return block;
+	//PERMUTATIONS
+	blockTmp = 0;
+	mask = 0x1;
+	for(j=0;j<8;j++)
+	{
+		valeurIndiceJ=(block & (mask << j)) >> j;
+		blockTmp |= valeurIndiceJ << permutations[j];
+	}
+	block = blockTmp;
+
+	return block;
 }
 
 unsigned char decrypt_block(unsigned char  block, unsigned short key, unsigned char substitutions[RANGE_VALUE_SUBST], unsigned char permutations[BLOCK_SIZE])
 {
-  unsigned char k[2];
+	unsigned char k[2];
 
-  memcpy((void*)k,(void*)&key,2*sizeof(unsigned char));
+	memcpy((void*)k,(void*)&key,2*sizeof(unsigned char));
 
-  unsigned char blockTmp = 0;
-  unsigned char mask =0;
+	unsigned char blockTmp = 0;
+	unsigned char mask =0;
+	unsigned char valeurIndiceJ=0;
+	int j;
 
-  //PERMUTATIONS
-  int j;
-  blockTmp = 0;
-  mask = 0x1;
-  for(j=0;j<BLOCK_SIZE;j++)
-  {
-    blockTmp |= (block & (mask << permutations[j]));
-  }
-  block = blockTmp;
+	//PERMUTATIONS
+	blockTmp = 0;
+	mask = 0x1;
+	for(j=0;j<BLOCK_SIZE;j++)
+	{
+		valeurIndiceJ=(block & (mask << j)) >> j;
+		blockTmp |= valeurIndiceJ << permutations[j];
+	}
+	block = blockTmp;
 
-  //SUBSTITUTIONS
-  blockTmp = 0;
-  mask = 0xf;
-  blockTmp |= substitutions[(block & mask)];
-  mask = mask << 4;
-  blockTmp |= (substitutions[(block & mask)>>4] << 4);
-  block = blockTmp;
+	//SUBSTITUTIONS
+	blockTmp = 0;
+	mask = 0xf;
+	blockTmp |= substitutions[(block & mask)];
+	mask = mask << 4;
+	blockTmp |= (substitutions[(block & mask)>>4] << 4);
+	block = blockTmp;
 
-  block = block ^ k[1];
+	block = block ^ k[1];
 
-  //PERMUTATIONS
-  blockTmp = 0;
-  mask = 0x1;
-  for(j=0;j<8;j++)
-  {
-    blockTmp |= (block & (mask << permutations[j]));
-  }
-  block = blockTmp;
+	//PERMUTATIONS
+	blockTmp = 0;
+	mask = 0x1;
+	for(j=0;j<8;j++)
+	{
+		valeurIndiceJ=(block & (mask << j)) >> j;
+		blockTmp |= valeurIndiceJ << permutations[j];
+	}
+	block = blockTmp;
 
-  //SUBSTITUTIONS
-  blockTmp = 0;
-  mask = 0xf;
-  blockTmp |= substitutions[(block & mask)];
-  mask = mask << 4;
-  blockTmp |= (substitutions[(block & mask)>>4] << 4);
-  block = blockTmp;
+	//SUBSTITUTIONS
+	blockTmp = 0;
+	mask = 0xf;
+	blockTmp |= substitutions[(block & mask)];
+	mask = mask << 4;
+	blockTmp |= (substitutions[(block & mask)>>4] << 4);
+	block = blockTmp;
 
-  block = block ^ k[0];
+	block = block ^ k[0];
 
-  return block;
+	return block;
 
 }
