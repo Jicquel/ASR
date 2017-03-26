a757c61b60ecc85fc26a1917032d621291329387
Jicquel Vincent <vincent.jicquel@gmail.com>
2017-03-26 15:32:47 +0200

    crackKey

--- TD-TP/TP17bis/crackKey.c
+++ TD-TP/TP17bis/crackKey.c
@@ -1,58 +1,96 @@
-#include "crypt.h"
+#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include "crypt.h"
+#include <unistd.h>
+#include <string.h>
+
 
 int main(int argc,char** argv)
 {
-  unsigned short key = 65251;
 
-  unsigned char block = 205;
+  if(argc != 6)
+  {
+    perror("exe <substitution file> <permutation file> <plain file> <cipher file>");
+    exit(1);
+  }
 
+  int fdSubst=open(argv[1],O_RDONLY);
+  if(fdSubst==-1)
+  {
+    perror("open subst");
+    exit(1);
+  }
+
+  int fdPerm=open(argv[2],O_RDONLY);
+  if(fdPerm==-1)
+  {
+    perror("open perm");
+    exit(1);
+  }
+
+  int fdPlain=open(argv[3],O_RDONLY);
+  if(fdPlain==-1)
+  {
+    perror("open plain");
+    exit(1);
+  }
+
+  int fdCipher=open(argv[4],O_RDONLY);
+  if(fdCipher==-1)
+  {
+    perror("open cipher");
+    exit(1);
+  }
+
+
+
+
+  //LECTURE PERMUTATIONS 
   unsigned char perm[8];
-  perm[0]=7;
-  perm[1]=1;
-  perm[2]=0;
-  perm[3]=4;
-  perm[4]=2;
-  perm[5]=6;
-  perm[6]=5;
-  perm[7]=3;
 
-  unsigned char permInverse[8];
-  int i;
-  for(i=0; i < 8 ; i++)
-  permInverse[perm[i]] = i;
+  memset(perm,0,8*sizeof(unsigned char));
+  if(read(fdPerm, (void*)perm,8*sizeof(unsigned char)) == -1)
+  {
+    perror("read perm");
+    exit(1);
+  }
 
+  //LECTURE SUBSTITUTIONS
   unsigned char substitutions[16];
-  substitutions[0]=3;
-  substitutions[1]=8;
-  substitutions[2]=14;
-  substitutions[3]=1;
-  substitutions[4]=12;
-  substitutions[5]=5;
-  substitutions[6]=10;
-  substitutions[7]=0;
-  substitutions[8]=2;
-  substitutions[9]=7;
-  substitutions[10]=9;
-  substitutions[11]=11;
-  substitutions[12]=4;
-  substitutions[13]=6;
-  substitutions[14]=15;
-  substitutions[15]=13;
 
-  unsigned char substitutionsInverse[16];
-  for(i=0; i < 16 ; i++)
-  substitutionsInverse[substitutions[i]] = i;
+  memset(substitutions,0,16*sizeof(unsigned char));
+  if(read(fdSubst, (void*)substitutions, 16*sizeof(unsigned char)) == -1)
+  {
+    perror("read subs");
+    exit(1);
+  }
 
-  printf("avant cryptage  : %u\n", block);
-  block = encrypt_block(block,key,substitutions,perm);
 
-  printf("apres cryptage  : %u\n", block);
-  block = decrypt_block(block,key,substitutionsInverse,permInverse);
+  //CRACKAGE
+  unsigned short key = 0;
+  unsigned char blockCrypted = 0;
+  unsigned char block=0;
 
-  printf("apres decryptage  : %u\n", block);
+  if(read(fdCipher, (void*) &blockCrypted, sizeof(unsigned char)) <= 0)
+  {
+    perror("read cipher");
+    exit(1);
+  }
 
-  return 1;
+  for(key = 0; key < 65536 ; key++)
+  {
+    block = encrypt_block(block,key,substitutions,perm);
+    if(block == blockCrypted)
+      {
+        printf("key : %u\n", key);
+        break;
+       }
+  }
+
+  return EXIT_SUCCESS;
 }
+
+
 
 
